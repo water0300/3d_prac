410b388dc1d04198c82cd1c18b1d52116684b743
water0300 <yuen.nathan@yahoo.com>
2022-06-12 18:11:26 -0700

    set up deps n imports

---
+++ 3d_prac_proj/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+    
+    void Start(){
+        
+    }
+
+    void Update(){
+
+    }
+}
